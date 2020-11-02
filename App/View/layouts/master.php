2ab08b6f9b7fe3daf38be57214658515f2350d19
andri-sudarmawijaya <andri.sudarmawijaya@gmail.com>
2020-11-03 01:57:14 +0700

    Replace css layout master

--- App/View/layouts/master.php
+++ App/View/layouts/master.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<title>@yield('title')</title>
 	<meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content=" ">
+  <meta name="author" content=" ">
+  
 	{!! $this->url->htmlLink( 'bootstrap.min.css') !!}
 	{!! $this->url->htmlLink( 'bootstrap-theme.min.css') !!}
 	{!! $this->url->htmlLink( 'main.css') !!}
 
+  <!-- Custom fonts for this template-->
+	{!! $this->url->htmlLink( 'fontawesome-free/css/all.min.css')  !!}
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+	{!! $this->url->htmlLink( 'sb-admin-2.min.css') !!}
+  
 </head>
 <body>
 	@include( 'includes.nav' )
